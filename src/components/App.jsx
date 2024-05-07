a58a70021ed5344950696ecb58f271a783c91dbc
Hassaaan1010 <hassaaan1010@gmail.com>
2024-05-07 18:23:05 +0530

    added basic notes functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  let [noteList, setNoteList] = useState([
+    { title: "asdf", content: "sadf a sdf saf" },
+  ]);
+
+  function addNote(obj) {
+    setNoteList((prevValue) => {
+      return [...prevValue, obj];
+    });
+    console.log(2);
+  }
+
+  function deleteNote(id) {
+    setNoteList((prevValue) => {
+      return prevValue.filter((val, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea addNote={addNote} />
+      {noteList.map((elem, index) => {
+        {
+          console.log(elem);
+        }
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={elem.title}
+            content={elem.content}
+            delete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  let [inputTitle, setInputTitle] = useState("");
+  let [inputText, setInputText] = useState("");
+
+  function handleChangeTitle() {
+    setInputTitle(event.target.value);
+  }
+
+  function handleChangeText() {
+    setInputText(event.target.value);
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          onChange={handleChangeTitle}
+          name="title"
+          placeholder="Title"
+          value={inputTitle}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={inputText}
+          onChange={handleChangeText}
+        />
+        <button
+          onClick={(event) => {
+            event.preventDefault();
+            props.addNote({ title: inputTitle, content: inputText });
+            console.log(1);
+          }}
+        >
+          Add
+        </button>
       </form>
     </div>
   );
